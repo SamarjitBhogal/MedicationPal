9896fbe5b456fd60e601521b6bea4ceaf8a25744
Samarjit Bhogal <samarjit.v.bhogal@gmail.com>
2024-03-06 12:11:14 -0800

    made log out mechanism

--- scripts/navSkeleton.js
+++ scripts/navSkeleton.js
@@ -1,11 +1,25 @@
 /**
  * This script is to help load the navbar and footer on any page we create.
- * 
- * TODO: implement firebase login auth to display full navbar and when not auth display limited navbar
  */
 
+// function loadSkeleton() {
+//     console.log($("#nav-placeholder").load("../components/navbar.html"));
+//     console.log($("#footer-placeholder").load("../components/footer.html"));
+// }
+// loadSkeleton();
+
 function loadSkeleton() {
-    console.log($("#nav-placeholder").load("../components/navbar.html"));
-    console.log($("#footer-placeholder").load("../components/footer.html"));
+
+    firebase.auth().onAuthStateChanged(function (user) {
+        if (user) {
+            // User is signed in.
+            console.log($("#nav-placeholder").load("../components/navbar.html"));
+            console.log($("#footer-placeholder").load("../components/footer.html"));
+        } else {
+            // No user is signed in.
+            console.log($("#nav-placeholder").load("../components/navbar_before_login.html"));
+            console.log($("#footer-placeholder").load("../components/footer.html"));
+        }
+    });
 }
 loadSkeleton();
--- scripts/script.js
+++ scripts/script.js
@@ -1,4 +1,12 @@
-function sayHello() {
-    
+//------------------------------------------------
+// Call this function when the "logout" button is clicked
+//-------------------------------------------------
+function logout() {
+    firebase.auth().signOut().then(() => {
+        // Sign-out successful.
+        console.log("logging out user");
+      }).catch((error) => {
+        console.log(error);
+        // An error happened.
+      });
 }
-//sayHello();
