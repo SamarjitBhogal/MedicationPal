ac8a9df6ed9aaa48eed10acbb39f82ddef7ea2a5
Samarjit Bhogal <samarjit.v.bhogal@gmail.com>
2024-03-11 14:50:25 -0700

    fixed login erroe

--- scripts/firebaseAPI_BBY06.js
+++ scripts/firebaseAPI_BBY06.js
@@ -17,54 +17,5 @@ var firebaseConfig = {
 //--------------------------------------------
 // Initialize Firebase
 const app = firebase.initializeApp(firebaseConfig);
-
-// Reference database
-var medicationFormDB = firebase.database().ref("medicationForm");
-
-// Event listener for form submission
-document.getElementById("medicationForm").addEventListener("submit", submitForm);
-
-// Prevent default action of form
-//when you refresh it doesnt automatically deletes everything (or something)
-function submitForm(e) {
-    e.preventDefault();
-
-    // Get inputs
-    var name = getElementval("name");
-    var type = getElementval("type");
-    var date = getElementval("date");
-    var time = getElementval("time");
-    var desc = getElementval("desc");
-
-    //call function
-    saveMessages(name, type, date, time, desc);   
-
-    //create alert
-    document.get
-}
-
-//save message to firebase
-const saveMessages = (name, type, date, time, desc) => {
-    //push these values into our database
-    var newMedicationForm = medicationFormDB.push();
-
-    newMedicationForm.set({
-        name : name,
-        type : type,
-        date : date,
-        time : time,
-        desc : desc,
-
-    });
-    //reset form
-    document.getElementById("medicationForm").reset();
-
-};
-
-// Get value of each field
-const getElementval = (id) => {
-    return document.getElementById(id).value;
-}
-
 const db = firebase.firestore();
 const storage = firebase.storage();
