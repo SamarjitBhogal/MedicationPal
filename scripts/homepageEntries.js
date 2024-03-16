2d66350d4d42b2788fd84929db03bfb8c5a3d556
Samarjit Bhogal <samarjit.v.bhogal@gmail.com>
2024-03-16 16:42:38 -0700

    finished implementing entry viewing on homepage

--- scripts/homepageEntries.js
+++ scripts/homepageEntries.js
@@ -2,43 +2,92 @@ function displayEntries() {
     firebase.auth().onAuthStateChanged(user => {
         userID = user.uid;
 
-    db.collection("MedicationInfo").where("user", "==", userID).get().then((userEntries) => {
-        var btnNum = 1;
-        userEntries.forEach((entry) => {
-            var name = entry.data().name;  
-            var type = entry.data().type; 
-            var date = entry.data().date;  
-            var time = entry.data().time; 
-            var status = entry.data().status; 
+        db.collection("MedicationInfo").where("user", "==", userID).get().then((userEntries) => {
+            var markerNum = 1;
+            var completedNum = 1;
+            userEntries.forEach((entry) => {
+                var name = entry.data().name;  
+                var type = entry.data().type; 
+                var date = entry.data().date;  
+                var time = entry.data().time; 
+                var status = entry.data().status; 
 
-            var tr = document.createElement("tr");
-            var td = markerBtn(btnNum);
-            tr.appendChild(td);
-            var tdName = document.createElement("td");
-            tdName.innerHTML = name;
-            tr.appendChild(tdName);
-            var tdType = document.createElement("td");
-            tdType.innerHTML = type;
-            tr.appendChild(tdType);
-            var tdTime = document.createElement("td");
-            tdTime.innerHTML = time;
-            tr.appendChild(tdTime);
-            var tdDate = document.createElement("td");
-            tdDate.innerHTML = date;
-            tr.appendChild(tdDate);
-
-            document.getElementById("hpage-upcoming-tb").appendChild(tr);
-            $("#marker-btn" + btnNum).load("../components/markerBtn.html");
-            btnNum++;
+                if (!status) {
+                    displayUpcoming(entry, markerNum);
+                    markerNum++;
+                } else {
+                    displayCompleted(entry, completedNum);
+                    completedNum++;
+                }            
+            });
         });
     });
-});
+}
+
+function displayUpcoming(e, n) {
+    var name = e.data().name;  
+    var type = e.data().type; 
+    var date = e.data().date;  
+    var time = e.data().time; 
+
+    var tr = document.createElement("tr");
+    var td = markerBtn(n);
+    tr.appendChild(td);
+    var tdName = document.createElement("td");
+    tdName.innerHTML = name;
+    tr.appendChild(tdName);
+    var tdType = document.createElement("td");
+    tdType.innerHTML = type;
+    tr.appendChild(tdType);
+    var tdTime = document.createElement("td");
+    tdTime.innerHTML = time;
+    tr.appendChild(tdTime);
+    var tdDate = document.createElement("td");
+    tdDate.innerHTML = date;
+    tr.appendChild(tdDate);
+
+    document.getElementById("hpage-upcoming-tb").appendChild(tr);
+    $("#marker-btn" + n).load("../components/markerBtn.html");
+}
+
+function displayCompleted(e, n) {
+    var name = e.data().name;
+    var type = e.data().type;
+    var date = e.data().date;
+    var time = e.data().time;
+
+    var tr = document.createElement("tr");
+    var td = undoBtn(n);
+    tr.appendChild(td);
+    var tdName = document.createElement("td");
+    tdName.innerHTML = name;
+    tr.appendChild(tdName);
+    var tdType = document.createElement("td");
+    tdType.innerHTML = type;
+    tr.appendChild(tdType);
+    var tdTime = document.createElement("td");
+    tdTime.innerHTML = time;
+    tr.appendChild(tdTime);
+    var tdDate = document.createElement("td");
+    tdDate.innerHTML = date;
+    tr.appendChild(tdDate);
+
+    document.getElementById("hpage-completed-tb").appendChild(tr);
+    $("#completed-btn" + n).load("../components/undoBtn.html");
 }
 
 function markerBtn(num) {
     var td = document.createElement("td");
     var markerDiv = document.createElement("div");
     markerDiv.setAttribute("id", "marker-btn" + num);
+    td.appendChild(markerDiv);
+    return td;
+}
+
+function undoBtn(num) {
+    var td = document.createElement("td");
+    var markerDiv = document.createElement("div");
+    markerDiv.setAttribute("id", "completed-btn" + num);
     td.appendChild(markerDiv);
     return td;
 }
