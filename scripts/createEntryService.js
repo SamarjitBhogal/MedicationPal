2f9b1c5a54a78189e413ff12670d98f934c94d62
Samarjit Bhogal <samarjit.v.bhogal@gmail.com>
2024-03-22 14:54:26 -0700

    made image uploading in create entries

--- scripts/createEntryService.js
+++ scripts/createEntryService.js
@@ -1,6 +1,10 @@
 /* Color changing on select for the day buttons: */
 const dayBtn = document.querySelectorAll(".day-btn");
 
+/* Holds the images uploaded be the user */
+var imageFile1;
+var imageFile2;
+
 dayBtn.forEach(element => {
     element.addEventListener('click', () => {
         if (element.getAttribute("aria-pressed") == "false") {
@@ -11,6 +15,22 @@ dayBtn.forEach(element => {
         element.style.color = "#f1faee";
     });
 });
+
+function listenUpload() {
+    console.log("file input change noticed.");
+    var input1 = document.getElementById("medImg-1");
+    var input2 = document.getElementById("medImg-2");
+
+    input1.addEventListener('change', (e) => {
+        imageFile1 = e.target.files[0];
+    });
+
+    input2.addEventListener('change', (e) => {
+        imageFile2 = e.target.files[0];
+    });
+}
+
+listenUpload();
 
 // Reference to the collection
 const colRef = db.collection('MedicationInfo');
@@ -55,6 +75,8 @@ document.getElementById('medicationForm').addEventListener('submit', function(ev
     })
     .then(function(docRef) {
         console.log("Document written with ID: ", docRef.id);
+        //upload the image to Storage on Firebase
+        uploadImage(docRef.id);
         // Reset the form
         document.getElementById('medicationForm').reset();
     })
@@ -63,6 +85,32 @@ document.getElementById('medicationForm').addEventListener('submit', function(ev
     });
 });
 
-function uploadImage() {
-    console.log("supposed to upload image.");
+function uploadImage(docID) {
+    console.log("Uploading images.");
+
+    if(imageFile1) {
+        putAndUpdate(imageFile1, docID);
+    } 
+    if(imageFile2) {
+        putAndUpdate(imageFile2, docID);
+    }
+}
+
+/* Puts the image in the Storage and updates the MedicationInfo doc with the proper ID */
+function putAndUpdate(img, docID) {
+    var fireStorage = storage.ref("images/" + docID + ".jpg");
+
+    fireStorage.put(img).then(() => {
+        console.log("put images.");
+        fireStorage.getDownloadURL().then((url) => {
+            console.log("downloaded URL.");
+            colRef.doc(docID).update({
+                "image": url
+            }).then(() => {
+                console.log("updated firestore.");
+            });
+        });
+    }).catch((error) => {
+        console.log("Image upload failed: " + error);
+    });
 }
