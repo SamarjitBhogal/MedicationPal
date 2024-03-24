e1b0d763ec5c14cca93a7e33635dcc00ce084e42
Ben Nguyen <bnguyen87@my.bcit.ca>
2024-03-24 15:57:28 -0700

    some changes to homepageentries.js

--- scripts/homepageEntries.js
+++ scripts/homepageEntries.js
@@ -27,6 +27,7 @@ function displayUpcoming(e, n) {
     var time = e.data().time;
     var status = e.data().status;
 
+    if (status === false) { // Check if status is false
         var tr = document.createElement("tr");
         var td = markerBtn(n);
         tr.appendChild(td);
@@ -45,7 +46,7 @@ function displayUpcoming(e, n) {
 
         document.getElementById("hpage-upcoming-tb").appendChild(tr);
         $("#marker-btn" + n).load("../components/markerBtn.html");
-}
+}}
 
 //e is entity, n is number in the current iteration
 function displayCompleted(e, n) {
@@ -55,6 +56,7 @@ function displayCompleted(e, n) {
     var time = e.data().time;
     var status = e.data().status;
 
+    if (status === true) { // Check if status is true
         var tr = document.createElement("tr");
         var td = undoBtn(n);
         tr.appendChild(td);
@@ -73,7 +75,7 @@ function displayCompleted(e, n) {
 
         document.getElementById("hpage-completed-tb").appendChild(tr);
         $("#completed-btn" + n).load("../components/undoBtn.html");
-
+}
 }
 
 
