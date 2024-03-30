d9494463d5993839f67c66d73e7160a50d571af6
jeven-on-road <yaojeven1999@gmail.com>
2024-03-30 14:44:19 -0700

    complete viewentries with modal -- Niki

--- scripts/readEntryService.js
+++ scripts/readEntryService.js
@@ -3,41 +3,74 @@
 //------------------------------------------------------------------------------
 function displayCardsDynamically(collection) {
     var userid;
+    // Get the modal
+    var modal = document.getElementById("myModal");
     firebase.auth().onAuthStateChanged(user => {
         userid = user.uid;
-        console.log(userid);   
-        
-    db.collection(collection).where("user","==",userid).get()   //the collection called "MedicationInfo"
-    //db.collection(collection).get()   //the collection called "MedicationInfo"
-        .then(allEntries => {
-            var i = 1;  //Optional: if you want to have a unique ID for each hike
-            allEntries.forEach(doc => { //iterate thru each doc
-                var name = doc.data().name;       // get value of the "name" key
-                var type = doc.data().type;  // get value of the "details" key
-                var date = doc.data().date;    //get unique ID to each hike to be used for fetching right image
-                var time = doc.data().time; //gets the length field
-                var desc = doc.data().desc; //gets the length field
-                
-                var tr = document.createElement('tr');
-                var th = tr.appendChild(document.createElement('th'));
-                th.innerHTML = i;
-                var td1 = tr.appendChild(document.createElement('td'));
-                td1.innerHTML = name;
-                var td2 = tr.appendChild(document.createElement('td'));
-                td2.innerHTML = type;
-                var td3 = tr.appendChild(document.createElement('td'));
-                td3.innerHTML = desc;
-                var td4 = tr.appendChild(document.createElement('td'));
-                td4.innerHTML = date;
-                var td5 = tr.appendChild(document.createElement('td'));
-                td5.innerHTML = time;
-                console.log(name);
-                document.getElementById(collection + "-go-here").appendChild(tr);
-                i++;   //Optional: iterate variable to serve as unique ID
+        console.log(userid);
+
+        db.collection(collection).where("user", "==", userid).get()
+            .then(allEntries => {
+                var i = 1;
+                allEntries.forEach(async doc => { //iterate thru each doc
+                    var name = doc.data().name;
+                    var type = doc.data().type;
+                    var desc = doc.data().desc;
+                    var scheInfo = doc.data().schedule;
+                    var date;
+                    var time;
+                    await db.collection("schedule").where(firebase.firestore.FieldPath.documentId(), '==', scheInfo).get().then(
+                        (userSchedules) => {
+                            userSchedules.forEach(
+                                (doc) => {
+                                    date = doc.data().days;
+                                    time = doc.data().time;
+                                    console.log(date + time);
+                                }
+                            )
+                        }
+                    );
+                    var tr = document.createElement('tr');
+                    tr.className = "medicationRow";
+                    tr.onclick = function () {
+                        document.getElementById("medicationMedal").innerHTML = desc;
+                        modal.style.display = "block";
+                    };
+                    var th = tr.appendChild(document.createElement('th'));
+                    th.innerHTML = i;
+                    var td1 = tr.appendChild(document.createElement('td'));
+                    td1.innerHTML = name;
+                    var td2 = tr.appendChild(document.createElement('td'));
+                    td2.innerHTML = type;
+                    var td4 = tr.appendChild(document.createElement('td'));
+                    td4.innerHTML = date;
+                    var td5 = tr.appendChild(document.createElement('td'));
+                    td5.innerHTML = time;
+                    console.log(name);
+                    document.getElementById(collection + "-go-here").appendChild(tr);
+
+                    i++;   //Optional: iterate variable to serve as unique ID
+                })
             })
-        })
     });
+    window.onclick = function (event) {
+        if (event.target == modal) {
+            modal.style.display = "none";
+        }
+    }
+    // When the user clicks on <span> (x), close the modal
+    var span = document.getElementsByClassName("close")[0];
+    span.onclick = function () {
+        modal.style.display = "none";
+    }
 }
 
 displayCardsDynamically("MedicationInfo");  //input param is the name of the collection
+
+
+
+
+
+
+
 
